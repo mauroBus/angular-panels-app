ffb71fcdc9713b0e83bf9cf6d5170345f7887bf3
mauroBus <maurobuselli@gmail.com>
2017-01-23 23:08:16 -0300

    Add dashboardPieChart controller

---
+++ src/app/pages/dashboard/pieCharts/dashboardPieChart.js
@@ -0,0 +1,15 @@
+/**
+ * @author maurobuselli@gmail.com
+
+ */
+(function () {
+  'use strict';
+
+  angular.module('AngularPanelsApp.pages.dashboard')
+      .service('dashboardPieChart', dashboardPieChart);
+
+  /** @ngInject */
+  function dashboardPieChart() {
+
+  }
+})();
