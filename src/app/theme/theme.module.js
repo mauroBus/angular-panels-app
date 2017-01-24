7e0782716e23a781bad5c68a5f1f4ff1df747294
mauroBus <maurobuselli@gmail.com>
2017-01-23 23:31:58 -0300

    Create basic theme module setup

---
+++ src/app/theme/theme.module.js
@@ -0,0 +1,17 @@
+/**
+ * @author maurobuselli@gmail.com
+ */
+(function () {
+  'use strict';
+
+  angular.module('AngularPanelsApp.theme', [
+      'toastr',
+      'chart.js',
+      'angular-chartist',
+      'angular.morris-chart',
+      'textAngular',
+      'AngularPanelsApp.theme.components',
+      'AngularPanelsApp.theme.inputs'
+  ]);
+
+})();
