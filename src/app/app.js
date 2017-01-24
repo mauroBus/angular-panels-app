49169b978ca4649cc908a2c926c51c7d17351c89
mauroBus <maurobuselli@gmail.com>
2017-01-23 23:04:06 -0300

    Basic app setup with index, 404 and reg pages

---
+++ src/app/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('AngularPanelsApp', [
+  'ngAnimate',
+  'ui.bootstrap',
+  'ui.sortable',
+  'ui.router',
+  'ngTouch',
+  'toastr',
+  'smart-table',
+  "xeditable",
+  'ui.slimscroll',
+  'ngJsTree',
+  'angular-progress-button-styles',
+
+  'AngularPanelsApp.theme',
+  'AngularPanelsApp.pages'
+]);
