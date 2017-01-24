4bec6f298c7a367e8b2322ffb206b31e55dd2f72
mauroBus <maurobuselli@gmail.com>
2017-01-23 23:18:36 -0300

    Add morris chart module

---
+++ src/app/pages/charts/morris/morris.module.js
@@ -0,0 +1,29 @@
+/**
+ * @author maurobuselli@gmail.com
+ */
+(function () {
+  'use strict';
+
+  angular.module('AngularPanelsApp.pages.charts.morris', [])
+    .config(routeConfig).config(function(baConfigProvider){
+      var layoutColors = baConfigProvider.colors;
+      Morris.Donut.prototype.defaults.backgroundColor = 'transparent';
+      Morris.Donut.prototype.defaults.labelColor = layoutColors.defaultText;
+      Morris.Grid.prototype.gridDefaults.gridLineColor = layoutColors.borderDark;
+      Morris.Grid.prototype.gridDefaults.gridTextColor = layoutColors.defaultText;
+    });
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+        .state('charts.morris', {
+          url: '/morris',
+          templateUrl: 'app/pages/charts/morris/morris.html',
+          title: 'Morris',
+          sidebarMeta: {
+            order: 300,
+          }
+        });
+  }
+
+})();
